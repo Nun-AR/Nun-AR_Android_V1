8cbcec06320881a266b873696b8504966e77dc8c
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-10-05 14:44:16 +0900

    [INIT] Inital Project

---
+++ app/src/main/java/com/nunar/nun_ar_android_v1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nunar.nun_ar_android_v1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
