a5457d97b6c8c984c2b45be9b01e336c0769e33f
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-10-13 22:54:37 +0900

    [ADD] Home View

---
+++ app/src/main/java/com/nunar/nun_ar_android_v1/adapter/RecentPostAdapter.kt
@@ -0,0 +1,58 @@
+package com.nunar.nun_ar_android_v1.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.nunar.nun_ar_android_v1.R
+import com.nunar.nun_ar_android_v1.databinding.ItemPostBinding
+import com.nunar.nun_ar_android_v1.model.response.PostResponse
+
+class RecentPostAdapter :
+    ListAdapter<PostResponse, RecentPostAdapter.RecentPostViewHolder>(RecentPostDiffUtilCallback) {
+
+    class RecentPostViewHolder(private val binding: ItemPostBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(post: PostResponse) {
+            binding.tvTitle.text = post.title
+            binding.tvWrittenDate.text = post.writer
+            binding.tvTag.text = post.tag
+            binding.btnBookmark.setOnClickListener {
+
+            }
+
+            Glide.with(binding.root.context)
+                .load(post.thumbnail)
+                .into(binding.ivThumbnail)
+
+        }
+
+    }
+
+    object RecentPostDiffUtilCallback : DiffUtil.ItemCallback<PostResponse>() {
+        override fun areItemsTheSame(oldItem: PostResponse, newItem: PostResponse) =
+            oldItem.postIdx == newItem.postIdx
+
+        override fun areContentsTheSame(oldItem: PostResponse, newItem: PostResponse) =
+            oldItem == newItem
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentPostViewHolder {
+        return RecentPostViewHolder(DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_post,
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: RecentPostViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+}
