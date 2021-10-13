a0f07f1b93ce3ad463638eb156010327f723984f
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-10-13 22:53:44 +0900

    [ADD} Post API

--- app/src/main/java/com/nunar/nun_ar_android_v1/model/Server.kt
+++ app/src/main/java/com/nunar/nun_ar_android_v1/model/Server.kt
@@ -1,7 +1,18 @@
 package com.nunar.nun_ar_android_v1.model
 
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.nunar.nun_ar_android_v1.NunARApplication
 import com.nunar.nun_ar_android_v1.model.api.Auth
+import com.nunar.nun_ar_android_v1.model.api.Post
+import com.nunar.nun_ar_android_v1.utils.tokenDataStore
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -11,9 +22,20 @@ object Server {
 
     private val AWS_URL = "http://3.37.250.4:8080/v1/"
 
+    class TokenInterceptor : Interceptor {
+        private val tokenKey = stringPreferencesKey("token")
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val token = runBlocking { NunARApplication.context?.tokenDataStore?.data?.first() }?.get(tokenKey)
+            val request =
+                chain.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
+            return chain.proceed(request)
+        }
+    }
+
     private val interceptor = HttpLoggingInterceptor()
     private val client = OkHttpClient.Builder()
         .addInterceptor(interceptor)
+        .addInterceptor(TokenInterceptor())
         .build()
 
     private val server = Retrofit.Builder()
@@ -24,4 +46,5 @@ object Server {
         .build()
 
     val authApi: Auth = server.create(Auth::class.java)
+    val postApi: Post = server.create(Post::class.java)
 }
