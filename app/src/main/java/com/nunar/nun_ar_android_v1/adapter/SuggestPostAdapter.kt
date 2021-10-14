3379c85d1798f78827a00762247d7eca3830aaa1
임재현 <ljh040513@naver.com>
2021-10-14 20:25:31 +0900

    [ADD] Post(details) view add

---
+++ app/src/main/java/com/nunar/nun_ar_android_v1/adapter/SuggestPostAdapter.kt
@@ -0,0 +1,46 @@
+package com.nunar.nun_ar_android_v1.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.nunar.nun_ar_android_v1.R
+import com.nunar.nun_ar_android_v1.adapter.callback.PostResponseDiffUtilCallback
+import com.nunar.nun_ar_android_v1.databinding.ItemSuggestBinding
+import com.nunar.nun_ar_android_v1.model.response.PostResponse
+
+class SuggestPostAdapter : ListAdapter<PostResponse, SuggestPostAdapter.SuggestPostViewHolder>(
+    PostResponseDiffUtilCallback) {
+
+    class SuggestPostViewHolder(private val binding: ItemSuggestBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: PostResponse) {
+            binding.suggestTitle.text = post.title
+            binding.suggestTag.text = post.tag
+            binding.suggestDate.text = post.writer
+            binding.suggestBookmark.isSelected = post.isBookmarks
+            binding.suggestBookmark.setOnClickListener {
+
+            }
+
+            Glide.with(binding.root.context)
+                .load("http://3.37.250.4:8080/image/${post.thumbnail}")
+                .into(binding.suggestImage)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuggestPostViewHolder {
+        return SuggestPostAdapter.SuggestPostViewHolder(DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_post,
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: SuggestPostViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
--- app/src/main/java/com/nunar/nun_ar_android_v1/view/PostFragment.kt
+++ app/src/main/java/com/nunar/nun_ar_android_v1/view/PostFragment.kt
@@ -5,14 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.nunar.nun_ar_android_v1.R
+import com.nunar.nun_ar_android_v1.adapter.SuggestPostAdapter
+import com.nunar.nun_ar_android_v1.databinding.FragmentPostBinding
+import com.nunar.nun_ar_android_v1.utils.NetworkStatus
+import com.nunar.nun_ar_android_v1.viewmodel.PostViewModel
 
 class PostFragment : Fragment() {
+
+    val viewModel: PostViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        val binding = DataBindingUtil.inflate<FragmentPostBinding>(inflater, R.layout.fragment_post, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val suggestPostAdapter = SuggestPostAdapter()
+        binding.postRecycler.adapter = suggestPostAdapter
+
+        viewModel.indexPostResult.observe(viewLifecycleOwner, Observer {
+            when(it) {
+                is NetworkStatus.Error -> Toast.makeText(requireContext(), "${it.throwable.message}", Toast.LENGTH_SHORT).show()
+                is NetworkStatus.Loading -> {
+
+                }
+                is NetworkStatus.Success -> {
+                    suggestPostAdapter.submitList(it.data)
+                }
+            }
+        })
+
+        return binding.root
     }
 }
