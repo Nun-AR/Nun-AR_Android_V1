24ce5056d46020b68805213b38b21f2d623df5c9
임재현 <ljh040513@naver.com>
2021-10-27 09:17:54 +0900

    [FIX] error fix
    FIX - 충돌로 인해 생긴 버그 수정

--- app/src/main/java/com/nunar/nun_ar_android_v1/view/WritePostFragment.kt
+++ app/src/main/java/com/nunar/nun_ar_android_v1/view/WritePostFragment.kt
@@ -1,21 +1,41 @@
 package com.nunar.nun_ar_android_v1.view
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.nunar.nun_ar_android_v1.R
+import com.nunar.nun_ar_android_v1.databinding.FragmentWritePostBinding
+import com.nunar.nun_ar_android_v1.utils.NetworkStatus
+import com.nunar.nun_ar_android_v1.viewmodel.WritePostViewModel
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
 
 class WritePostFragment : Fragment() {
+
+    private val CODE_GET_FILE = 1001
+    private val CODE_GET_IMAGE = 1001
+
+    val viewModel: WritePostViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-<<<<<<< Updated upstream
-        return inflater.inflate(R.layout.fragment_write_post, container, false)
-=======
         val binding = DataBindingUtil.inflate<FragmentWritePostBinding>(inflater,
             R.layout.fragment_write_post,
             container,
@@ -40,8 +60,10 @@ class WritePostFragment : Fragment() {
         }
 
         viewModel.uploadFileResult.observe(viewLifecycleOwner, Observer {
-            when(it){
-                is NetworkStatus.Error -> Toast.makeText(requireContext(), "${it.throwable.message}", Toast.LENGTH_SHORT).show()
+            when (it) {
+                is NetworkStatus.Error -> Toast.makeText(requireContext(),
+                    "${it.throwable.message}",
+                    Toast.LENGTH_SHORT).show()
                 is NetworkStatus.Loading -> {
 
                 }
@@ -53,8 +75,10 @@ class WritePostFragment : Fragment() {
         })
 
         viewModel.uploadImageResult.observe(viewLifecycleOwner, Observer {
-            when(it){
-                is NetworkStatus.Error -> Toast.makeText(requireContext(), "${it.throwable.message}", Toast.LENGTH_SHORT).show()
+            when (it) {
+                is NetworkStatus.Error -> Toast.makeText(requireContext(),
+                    "${it.throwable.message}",
+                    Toast.LENGTH_SHORT).show()
                 is NetworkStatus.Loading -> {
 
                 }
@@ -85,7 +109,7 @@ class WritePostFragment : Fragment() {
                     val uri: Uri = getRealPathFromUri(data.data!!)
                     file = File(getRealPathFromUri(uri).path)
 
-                   val fileBody = RequestBody.create("image/jpeg".toMediaTypeOrNull(), file.path)
+                    val fileBody = RequestBody.create("image/jpeg".toMediaTypeOrNull(), file.path)
                     val filePart = MultipartBody.Part.createFormData("file",
                         file.path,
                         fileBody)
@@ -110,6 +134,5 @@ class WritePostFragment : Fragment() {
         cursor?.close()
 
         return Uri.fromFile(File(picturePath ?: ""))
->>>>>>> Stashed changes
     }
 }
