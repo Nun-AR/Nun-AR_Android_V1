41eea5b55e88c8c125548e6c0a7a679bfd94eddb
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-10-31 19:24:20 +0900

    [FIX] Change API
    Path -> Query Parameter

--- app/src/main/java/com/nunar/nun_ar_android_v1/model/api/Post.kt
+++ app/src/main/java/com/nunar/nun_ar_android_v1/model/api/Post.kt
@@ -5,6 +5,7 @@ import com.nunar.nun_ar_android_v1.model.response.PostResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Post {
 
@@ -17,7 +18,7 @@ interface Post {
     @GET("post/{postIdx}")
     fun getPostByIdx(@Path("postIdx") postIdx: Int): Single<BaseResponse<PostResponse>>
 
-    @GET("post/search/{searchWord}")
-    fun getSearchResult(@Path("searchWord") searchWord: String): Single<BaseResponse<List<PostResponse>>>
+    @GET("post/search")
+    fun getSearchResult(@Query("searchWord") searchWord: String): Single<BaseResponse<List<PostResponse>>>
 
 }
