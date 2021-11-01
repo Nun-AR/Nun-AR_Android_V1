cef0ba66787eb8b5446044ad94f1823fe84f0285
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-01 16:38:15 +0900

    [ADD] UserInfo View

--- app/src/main/java/com/nunar/nun_ar_android_v1/view/HomeFragment.kt
+++ app/src/main/java/com/nunar/nun_ar_android_v1/view/HomeFragment.kt
@@ -80,6 +80,10 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_home_fragment_to_write_post_fragment)
         }
 
+        binding.btnUserInfo.setOnClickListener {
+            findNavController().navigate(R.id.action_home_fragment_to_user_info_fragment)
+        }
+
         return binding.root
     }
 
